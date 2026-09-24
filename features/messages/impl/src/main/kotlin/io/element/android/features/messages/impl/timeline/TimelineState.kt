251cd6f1286c336dc47ccc41ca3a16ba3bfcfa1e
Franco OsmAnd Developer <franco@osmand-dev.local>
2026-09-23 23:44:15 -0300

    fix(quality): remove default parameter value in TimelineState for Konsist

--- features/messages/impl/src/main/kotlin/io/element/android/features/messages/impl/timeline/TimelineState.kt
+++ features/messages/impl/src/main/kotlin/io/element/android/features/messages/impl/timeline/TimelineState.kt
@@ -35,7 +35,7 @@ data class TimelineState(
     val displayThreadSummaries: Boolean,
     val displayJumpToUnread: Boolean,
     val jumpToUnread: JumpToUnreadState,
-    val hasUnreadMessages: Boolean = false,
+    val hasUnreadMessages: Boolean,
     val isManualReadReceiptsEnabled: Boolean,
     val isShowManualReadBannerEnabled: Boolean,
     val isShowManualReadBottomEnabled: Boolean,
